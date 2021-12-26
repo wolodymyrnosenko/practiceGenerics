bde6c024703d05c59191b439124f0034cdbdfd27
wolodymyrnosenko <wolodymyrnosenko@gmail.com>
2021-12-26 16:25:39 +0200

    fixed due to task

--- src/main/java/org/itstep/List.java
+++ src/main/java/org/itstep/List.java
@@ -3,12 +3,12 @@ package org.itstep;
 import java.util.Arrays;
 
 // FIXME: обобщенный тип должен быть классом расширяющим интерфейс Comparable<T>
-class List<T> {
+class List<T extends Comparable<T>>{
     private T[] arrays;
 
     public List() {
         // FIXME: создать массив типов Comparable
-        this.arrays = (T[]) new Object[0];
+        this.arrays = (T[]) new Comparable[0];
     }
 
     public void add(T item) {
@@ -23,22 +23,22 @@ class List<T> {
     public T min() {
         T min = arrays[0];
         // FIXME: реализовать поиск наименьшего элемента используя метод compareTo интерфейса Comparable<T>
-        /*for(T item: arrays) {
-            if(item < min) {
+        for(T item: arrays) {
+            if(item.compareTo(min) < 0) {
                 min = item;
             }
-        }*/
+        }
         return min;
     }
 
     public T max() {
         T max = arrays[0];
         // FIXME: реализовать поиск наибольшего элемента используя метод compareTo интерфейса Comparable<T>
-        /*for(T item: arrays) {
-            if(item > max) {
+        for(T item: arrays) {
+            if(item.compareTo(max) > 0) {
                 max = item;
             }
-        }*/
+        }
         return max;
     }
 }
--- src/main/java/org/itstep/Tea.java
+++ src/main/java/org/itstep/Tea.java
@@ -1,17 +1,27 @@
 package org.itstep;
 
 // FIXME: реализовать интерфейс Comparable<Tea>. Метод compareTo() должен производить сравнение по цене
-public class Tea {
-    private final String name;
-    private final double price;
+public class Tea implements Comparable<Tea>{
+//    private final String name;
+//    private final double price;
+    private String name;
+    private double price;
 
     public Tea(String name, double price) {
         this.name = name;
         this.price = price;
     }
+    public double getPrice(){
+        return price;
+    }
 
     @Override
     public String toString() {
         return "Чай: " + name + " Цена: " + price;
+    }
+
+    @Override
+    public int compareTo(Tea o) {
+        return (int)(this.getPrice()-o.getPrice());
     }
 }
